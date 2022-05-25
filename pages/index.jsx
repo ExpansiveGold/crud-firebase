71f9604a08f5482fe65a104e22e0f50043934688
ExpansiveGold <61693862+ExpansiveGold@users.noreply.github.com>
2022-05-25 10:05:36 -0300

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,40 +2,138 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 //importar as configurações do firebase
-import { app, db } from '../services/firebase';
-import { collection, addDoc } from 'firebase/firestore';
-import { useState } from 'react';
+import { app, database } from '../services/firebase'
+import { collection, addDoc, getDocs, orderBy, query } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
 
 //definir a coleção
-const contato = collection(db,'contato');
+const contato = collection(database,'contato')
 
 export default function Home() {
+
   //hooks
-  const [nome, SetNome]=useState('')
-  const [email, SetEmail]=useState('')
-  const [Telefone, SetTelefone]=useState('')
-  const [mensagem, SetMensagem]=useState('')
+  const[nome, SetNome]=useState('')
+  const[email, SetEmail]=useState('')
+  const[telefone, SetTelefone]=useState('')
+  const[mensagem, SetMensagem]=useState('')
+
+  //create
+  const create = ()=>{
+    addDoc(contato,
+      {
+        nome:nome,
+        email:email,
+        telefone:telefone,
+        mensagem:mensagem
+      }).then(()=>{
+        SetNome('')
+        SetEmail('')
+        SetTelefone('')
+        SetMensagem('')
+        read()
+      })
+  }
+  
+
+  //read
+  const[lista, setLista]=useState([])
+  
+  const read = ()=>{
+    getDocs(query(contato, orderBy('nome')))
+    .then((data)=>{
+      setLista(data.docs.map((item)=>{
+        return{...item.data(),id:item.id}
+      }))
+    })
+  }
+
+  //mostrar os documentos ao atualizar a pagina
+  useEffect(()=>{
+    read()
+  },[])
 
   return (
     <>
-    <Head>
-      <title>Crud simples com Firestore</title>
-    </Head>
-    <main className="container">
-      <div className="row"></div>
-      <div className="col-md">
-        <h3 className='text-center'>Cadastrar</h3>
-        <input type="text" placeholder='Nome' className='form-control' required />
-        <input type="email" placeholder='Email' className='form-control' required />
-        <input type="tel" placeholder='Telefone' className='form-control' required />
-        <textarea placeholder='Mensagem' className='form-control' required></textarea>
-        <input type="button" value="Salvar" className='btn btn-outline-dark form-control'/>
-      </div>
-      <div className="col-md">
-        <h3 className='text-center'>Exibir</h3>
-        
-      </div>
-    </main>
+      <Head>
+        <title>Crud simples com firestore</title>
+      </Head>
+
+      <main className="container">
+        <div className="row">
+          <div className="col-md">
+            <div className="text-center">
+              <h3>Cadastrar</h3>
+            </div>
+
+            <input 
+              type="text" 
+              placeholder="Nome" 
+              className="form-control" 
+              required 
+              onChange={event=>SetNome(event.target.value)}
+              value={nome}
+            /> 
+            <input 
+              type="email" 
+              placeholder="Email" 
+              className="form-control" 
+              required 
+              onChange={event=>SetEmail(event.target.value)}
+              value={email}
+            /> 
+            <input 
+              type="tel" 
+              placeholder="Telefone" 
+              className="form-control" 
+              required 
+              onChange={event=>SetTelefone(event.target.value)}
+              value={telefone}
+            /> 
+            <textarea 
+              placeholder="Mensagem" 
+              className="form-control" 
+              required 
+              onChange={event=>SetMensagem(event.target.value)}
+              value={mensagem}
+              />
+            <input 
+              type="button" 
+              value="Salvar" 
+              className="btn btn-outline-dark form-control" 
+              onClick={create}
+              
+              />
+          </div>
+          
+          <div className="col-md">
+            <h3 className="text-center">Exibir</h3>
+            {lista.map((lista)=>{
+              return(
+                <>
+                <div className="card">
+                  <div className="card-header bg-dark text-light">
+                    Id: {lista.id}
+                  </div>
+                  <div className="card-body">
+                    <p className="card-title text-info">Nome: {lista.nome}</p>
+                    <p className="card-subtitle">Email: {lista.email}</p>
+                    <p className="card-subtitle">Telefone: {lista.telefone}</p>
+                    <p className="card-subtitle">Mensagem: {lista.mensagem}</p>
+                  </div>
+                  <div className="card-footer">
+                    <div className="input-group">
+                      <input type="button" value="Alterar" className="btn btn-outline-warning form-control" />
+                      <input type="button" value="Excluir" className="btn btn-outline-danger form-control" />
+                    </div>
+                  </div>
+                </div>
+                  
+                </>
+              )
+            })}
+          </div>
+        </div>
+      </main>
     </>
   )
 }
--- services/firebase.jsx
+++ services/firebase.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
+// Importar os recursos do firebase
+import {getFiresote, getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { app, db };
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
